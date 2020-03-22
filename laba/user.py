ee17836ee813643bc04573e9e669842813ef24bd
snake-whisper <snake-whisper@web-utils.eu>
2020-03-22 12:33:12 +0100

    user storage optimation

--- laba/user.py
+++ laba/user.py
@@ -17,30 +17,30 @@ class LoginUser():
         if not hasattr(g, 'redis'):
             g.redis = redis.Redis(host='localhost', port=6379, db=0)
         #chkCred
-        res = self.queryOne("""SELECT
+        self.__values = self.queryOne("""SELECT
                 id, username, firstName, lastName, email, ctime, atime, status, icon, enabled
                 FROM users
                 WHERE
                 (username = %s or email = %s)
                 AND
                 password = SHA2(%s, 256)""", (username, username, passwd))
-        if not res:
+        if not self.__values:
             raise BadUserCredentials(username)
         
-        if not res["enabled"]:
+        if not self.__values["enabled"]:
             raise UserDisabled(username)
 
         self.__changed = {}
 
-        self.__id = res["id"]
-        self.__username = res["username"]
-        self.__firstName = res["firstName"]
-        self.__lastName = res["lastName"]
-        self.__email = res["email"]
-        self.__ctime = res["ctime"]
-        self.__atime = res["atime"] #last seen
-        self.__status = res["status"]
-        self.__icon = res["icon"]
+        #self.__id = self.values["id"]
+        #self.__username = self.values["username"]
+        #self.__firstName = self.values["firstName"]
+        #self.__lastName = self.values["lastName"]
+        #self.__email = self.values["email"]
+        #self.__ctime = self.values["ctime"]
+        #self.__atime = self.values["atime"] #last seen
+        #self.__status = self.values["status"]
+        #self.__icon = self.values["icon"]
         self.__uuid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
 
         session['uuid'] = self.__uuid
@@ -63,7 +63,7 @@ class LoginUser():
 
     @property
     def id(self):
-        return self.__id
+        return self.__values["id"]
     
     @property
     def uuid(self):
@@ -71,11 +71,11 @@ class LoginUser():
     
     @property
     def username(self):
-        return self.__username
+        return self.__values["username"]
     @username.setter
     def username(self, value):
-        if self.__username != value:
-            self.__username = value
+        if self.__values["username"] != value:
+            self.__values["username"] = value
             self.__changed['username'] = value
 
     @property
@@ -83,62 +83,62 @@ class LoginUser():
         return self.__firstName
     @firstName.setter
     def firstName(self, value):
-        if self.__firstName != value:
-            self.__firstName = value
+        if self.__values["firstName"] != value:
+            self.__values["firstName"] = value
             self.__changed['firstName'] = value
 
     @property
     def lastName(self):
-        return self.__lastName
+        return self.__values["lastName"]
     @lastName.setter
     def lastName(self, value):
-        if self.__lastName != value:
-            self.__lastName = value
+        if self.__values["lastName"] != value:
+            self.__values["lastName"] = value
             self.__changed['lastName'] = value
 
     @property
     def email(self):
-        return self.__email
+        return self.__values["email"]
     @email.setter
     def email(self, value):
-        if self.__email != value:
-            self.__email = value
+        if self.__values["email"] != value:
+            self.__values["email"] = value
             self.__changed['email'] = value
 
     @property
     def ctime(self):
-        return self.__ctime
+        return self.__values["ctime"]
     @ctime.setter
     def ctime(self, value):
-        if self.__ctime != value:
-            self.__ctime = value
+        if self.__values["ctime"] != value:
+            self.__values["ctime"] = value
             self.__changed['ctime'] = value
     
     @property
     def atime(self):
-        return self.__atime
+        return self.__values["atime"]
     @atime.setter
     def atime(self, value):
-        if self.__atime != value:
-            self.__atime = value
+        if self.__values["atime"] != value:
+            self.__values["atime"] = value
             self.__changed['atime'] = value
 
     @property
     def status(self):
-        return self.__status
+        return self.__values["status"]
     @status.setter
     def status(self, value):
-        if self.__status != value:
-            self.__status = value
+        if self.__values["status"] != value:
+            self.__values["status"] = value
             self.__changed['status'] = value
 
     @property
     def icon(self):
-        return self.__icon
+        return self.__values["icon"]
     @icon.setter
     def icon(self, value):
-        if self.__icon != value:
-            self.__icon = value
+        if self.__values["icon"] != value:
+            self.__values["icon"] = value
             self.__changed['icon'] = value
     
     def changePwd (self, old, new):
@@ -148,21 +148,17 @@ class LoginUser():
     
     def commit2db(self):
         if self.__changed:
-            sql="UPDATE users SET {0} WHERE users.id = {1}".format(", ".join(i+"=%s" for i in self.__changed.keys()), self.__id)
+            sql="UPDATE users SET {0} WHERE users.id = {1}".format(", ".join(i+"=%s" for i in self.__changed.keys()), self.__values["id"])
             self.query(sql, tuple(self.__changed.values()))
+            print("updated")
     
-    def commit2redis(self):
-        vars = {"id" : self.__id,
-        "username" : self.__username,
-        "firstName" : self.__firstName,
-        "lastName" : self.__lastName,
-        "email" : self.__email,
-        "ctime" : str(self.__ctime),
-        "atime" : str(self.__atime),
-        "status" : self.__status,
-        "icon" : self.__icon}
-        print (vars)
-        g.redis.set(self.__uuid, json.dumps(vars))
+    def __serialize(self):
+        self.__values['atime'] = str(self.__values['atime']) #Keep private! It's changing self.__value!!!
+        self.__values['ctime'] = str(self.__values['ctime'])
+        return json.dumps(self.__values)
+    
+    def commit2redis(self):        
+        g.redis.set(self.__uuid, self.__serialize(), 60)
 
     def __del__(self):
         self.commit2db()
