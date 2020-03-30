df59b19c94f063d23f60411792726093cd8a551b
snake-whisper <snake-whisper@web-utils.eu>
2020-03-31 00:53:01 +0200

    .

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/snake-whisper/laba",
     install_requires=["pymysql", "redis", "Flask", "Flask-SocketIO", "validate-email", "eventlet"],
     include_package_data=True,
-    packages=['laba-server'],
+    packages=['laba'],
 #    classifiers=[
 #        "Programming Language :: Python :: 3",
 #        "License :: OSI Approved :: MIT License",
