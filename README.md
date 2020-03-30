318766502bd7e9e07f562d59c1a1257c04c6970a
snake-whisper <snake-whisper@web-utils.eu>
2020-03-30 23:35:14 +0200

    build first attemp

---
+++ README.md
@@ -0,0 +1,7 @@
+Laba-Server
+===========
+
+This server implements a simple chat server app written in python/flask.
+
+#TODO Add Documentation
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="laba-server", # Replace with your own username
+    name="laba-server",
     version="0.0.1",
     author="snake-whipser",
     author_email="snake-whisper@web-utils.eu",
@@ -12,16 +12,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/snake-whisper/laba",
-    packages=setuptools.find_packages(),
+    install_requieres=["pymysql", "redis", "Flask", "Flask-SocketIO", "validate-email", "eventlet"],
+    install_requieres=True,
+    packages=['laba-server']
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-	'flask',
-	'flask-socketio',
-	'redis',
-	'pymysql'],
     python_requires='>=3.6',
 )
