bdab5e56e98e172c06f0c7756478a10bf45a86bd
snake-whisper <snake-whisper@web-utils.eu>
2020-04-03 19:00:46 +0000

    .

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export FLASK_APP=app.py
+
 apt-get update
 apt-get install -y --no-install-recommends python3-pip python3-venv git
 python3 -m venv venv
@@ -7,6 +11,5 @@ python3 -m venv venv
 pip3 install wheel
 pip3 install --editable .
 cd laba
-export FLASK_APP=app.py
 flask initDB
 #deactivate
